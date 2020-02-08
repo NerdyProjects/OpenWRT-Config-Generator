23d48ba330e28fdba025a0669b6d7ec09acf2700
chandi <git@chandi.it>
2020-02-08 16:50:26 +0100

    customizeable paths

--- generate_config.py
+++ generate_config.py
@@ -1,5 +1,23 @@
 import yaml
 import os
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Generate wifi configurations for OpenWRT Access Points')
+parser.add_argument(
+    '--config',
+    default="config.yaml"
+)
+parser.add_argument(
+    'hostname',
+    help=''
+)
+parser.add_argument(
+    'outdir',
+    help='output directory (/etc/config on the device)',
+)
+args = parser.parse_args()
+
 
 def collect_bssids(config, ssid):
     bssids = []
@@ -54,56 +72,65 @@ class Config:
         return '\n'.join(self.config)
 
 
-with open("config.yaml", 'r') as stream:
+with open(args.config, 'r') as stream:
     config = yaml.safe_load(stream)
 
-print(config)
 wifi_config = {}
 for wifi in config['wifis']:
     if wifi['mode'] == 'ap':
         wifi_config[wifi['ssid']] = wifi.copy()
         wifi_config[wifi['ssid']]['bssids'] = collect_bssids(config, wifi['ssid'])
 
-print(wifi_config)
 configs = {}
-for ap in config['accesspoints']:
-    c = Config()
-    radio_i = 0
-    for radio in ap['radios']:
-        device = 'radio{}'.format(radio_i)
-        c.append_config_section('wifi-device', device)
-        c.append_config_option('type', 'mac80211')
-        c.append_config_option('legacy_rates', '0')
-        c.append_config_option('country', 'DE')
-        copy_options = ['htmode', 'channel', 'path', 'hwmode']
-        for opt in copy_options:
-            if opt in radio:
-                c.append_config_option(opt, radio[opt])
-        for wifi in radio['wifis']:
-            id = wifi['ssid']
-            c.append_config_section('wifi-iface')
-            c.append_config_option('device', device)
-            c.append_config_option('bssid', wifi['bssid'])
-            copy_options = ['mode', 'network', 'encryption', 'auth_port', 'auth_secret', 'auth_server', 'dynamic_vlan', 'vlan_tagged_interface', 'vlan_bridge', 'vlan_naming',
-        'dtim_period', 'ieee80211r', 'mobility_domain', 'ft_over_ds', 'ft_psk_generate_local', 'pmk_r1_push', 'ft_bridge', 'key', 'ssid', 'rsn_preauth']
-            for opt in copy_options:
-                if opt in wifi_config[id]:
-                    c.append_config_option(opt, wifi_config[id][opt])
-            if 'ieee80211r' in wifi_config[id] and 'ft_exchange_aes' in wifi_config[id]:
-                r0kh = generate_r0kh_list(wifi_config[id]['bssids'], wifi['bssid'], wifi_config[id]['ft_exchange_aes'])
-                r1kh = generate_r1kh_list(wifi_config[id]['bssids'], wifi['bssid'], wifi_config[id]['ft_exchange_aes'])
-                for i in r0kh:
-                    c.append_config_list_item('r0kh', i)
-                for i in r1kh:
-                    c.append_config_list_item('r1kh', i)
-        radio_i += 1
-    configs[ap['name']] = c
 
-for name, ap in configs.items():
-    path = 'generated/{}/config'.format(name)
-    os.makedirs(path, 0o755, True)
-    file = open('{}/wireless'.format(path), 'w')
-    file.writelines(ap.dump())
-    file.close()
-    print('config for {}'.format(name))
-    print(ap.dump())
+
+ap = None
+for a in config['accesspoints']:
+    if a['name'] == args.hostname:
+        ap = a
+        break
+
+if not ap:
+    print("no access point found. maybe hostname is invalid?")
+    sys.exit(1)
+
+
+
+c = Config()
+radio_i = 0
+for radio in ap['radios']:
+    device = 'radio{}'.format(radio_i)
+    c.append_config_section('wifi-device', device)
+    c.append_config_option('type', 'mac80211')
+    c.append_config_option('legacy_rates', '0')
+    c.append_config_option('country', 'DE')
+    copy_options = ['htmode', 'channel', 'path', 'hwmode']
+    for opt in copy_options:
+        if opt in radio:
+            c.append_config_option(opt, radio[opt])
+    for wifi in radio['wifis']:
+        id = wifi['ssid']
+        c.append_config_section('wifi-iface')
+        c.append_config_option('device', device)
+        c.append_config_option('bssid', wifi['bssid'])
+        copy_options = ['mode', 'network', 'encryption', 'auth_port', 'auth_secret', 'auth_server', 'dynamic_vlan', 'vlan_tagged_interface', 'vlan_bridge', 'vlan_naming',
+    'dtim_period', 'ieee80211r', 'mobility_domain', 'ft_over_ds', 'ft_psk_generate_local', 'pmk_r1_push', 'ft_bridge', 'key', 'ssid', 'rsn_preauth']
+        for opt in copy_options:
+            if opt in wifi_config[id]:
+                c.append_config_option(opt, wifi_config[id][opt])
+        if 'ieee80211r' in wifi_config[id] and 'ft_exchange_aes' in wifi_config[id]:
+            r0kh = generate_r0kh_list(wifi_config[id]['bssids'], wifi['bssid'], wifi_config[id]['ft_exchange_aes'])
+            r1kh = generate_r1kh_list(wifi_config[id]['bssids'], wifi['bssid'], wifi_config[id]['ft_exchange_aes'])
+            for i in r0kh:
+                c.append_config_list_item('r0kh', i)
+            for i in r1kh:
+                c.append_config_list_item('r1kh', i)
+    radio_i += 1
+
+
+print("./wireless")
+os.makedirs(args.outdir, 0o755, True)
+file = open('{}/wireless'.format(args.outdir), 'w')
+file.writelines(c.dump())
+file.close()
+
